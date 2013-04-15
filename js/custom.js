e0f0901e38995958b5d589bf86856b693efbc8d7
Marcus <marcus@mwermuth.com>
2013-04-15 21:31:02 +0200

    Pre release

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,36 @@
   $('#video').click(function(){
 		if($("#video-content").height() === 656){
-			$(".bg-down").css({'background-position': '0px 0px'});
+			$(".bg-down").css({'margin-top': '0px'});
 			$(".video-down").css("height","0px");
 			$("#video-content").empty();
-			$("body").addClass("fixed-top-bar");
 		}
 		else{
-			$("body").removeClass("fixed-top-bar");
-			$(".bg-down").css({'background-position': '0px 656px'});
+			$(".bg-down").css({'margin-top': '656px'});
+			$(".video-down").css("height","656px");
+			$("#video-content").append('<div class="container"></div>');
+			
+			$('#video-content div.container').delay(400).queue(function () {
+
+					$(this).append('<div class="row"><div class="span16"><div class="video"><iframe src="http://player.vimeo.com/video/8709313?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="960" height="540" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div></div></div>');
+				$(this).append('<div class="row"  style="padding-right:35px;"><div class="span16"><div class="video-close"><a href="#" id="video-close"><img src="images/video-close.png"/></div></div></div>');
+			});
+		}
+  });
+  
+  $('#video-play').click(function(){
+		if($("#video-content").height() === 656){
+			$(".bg-down").css({'margin-top': '0px'});
+			$(".video-down").css("height","0px");
+			$("#video-content").empty();
+		}
+		else{
+			$(".bg-down").css({'margin-top': '656px'});
 			$(".video-down").css("height","656px");
 			$("#video-content").append('<div class="container"></div>');
 			
 			$('#video-content div.container').delay(400).queue(function () {
 				$(this).append('<div class="row"><div class="span16"><div class="video"><iframe src="http://player.vimeo.com/video/8709313?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="960" height="540" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div></div></div>');
-				$(this).append('<div class="row"><div class="span16"><div class="video-close"><a href="#" id="video-close"><img src="images/video-close.png"/></div></div></div>');
+				$(this).append('<div class="row"  style="padding-right:35px;"><div class="span16"><div class="video-close"><a href="#" id="video-close"><img src="images/video-close.png"/></div></div></div>');
 			});
 		}
   });
@@ -22,11 +39,9 @@
 		if($("#video-content").height() === 656){
 			console.log("TEST");
 			
-			$("body").removeClass("fixed-top-bar");
-			$(".bg-down").css({'background-position': '0px 0px'});
+			$(".bg-down").css({'margin-top': '0px'});
 			$(".video-down").css("height","0px");
 			$("#video-content").empty();
-			$("body").addClass("fixed-top-bar");
 			
 		}
 	});
@@ -34,7 +49,10 @@
 	
   $("#haltestellen").mouseover(function() {
 		$("#haltestellen h3").css("color", "#00d9bd");
-		$("#iphone-center").append('<img src="images/howitworks-haltestelleniphone.png" id="iphone-over" style="position:absolute;width:470px;display:block;height:702px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		//$("#iphone-center").append('<img src="images/howitworks-haltestelleniphone.png" id="iphone-over" style="position:absolute;width:470px;display:block;height:702px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		
+		$("#one").removeClass("hidden").addClass("screenhot-show");
+		$("#arrows").css("z-index","-1").css("background-image","url(images/arrows-haltestelle.png)");
 		
 		$("#datepicker").css("opacity","0.4");
 		$("#suche").css("opacity","0.4");
@@ -42,8 +60,9 @@
 		$("#nextrbutton").css("opacity","0.4");
 		
   }).mouseout(function(){
-		$("#iphone-over").remove();
+		$("#one").addClass("hidden").removeClass("screenhot-show");
 		$("#haltestellen h3").css("color", "#000000");
+		$("#arrows").css("z-index","0").css("background-image","url(images/arrows.png)");
 		
 		$("#datepicker").css("opacity","");
 		$("#suche").css("opacity","");
@@ -53,16 +72,19 @@
 	
   $("#datepicker").mouseover(function() {
 		$("#datepicker h3").css("color", "#00d9bd");
+		$("#arrows").css("z-index","-1").css("background-image","url(images/arrows-datepicker.png)");
 		
-		$("#iphone-center").append('<img src="images/howitworks-datepickeriphone.png" id="iphone-over" style="position:absolute;width:470px;display:block;height:702px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		$("#two").removeClass("hidden").addClass("screenhot-show");
+
 		$("#haltestellen").css("opacity","0.4");
 		$("#suche").css("opacity","0.4");
 		$("#bringmehome").css("opacity","0.4");
 		$("#nextrbutton").css("opacity","0.4");
   }).mouseout(function(){
 		$("#datepicker h3").css("color", "#000000");
+		$("#arrows").css("z-index","0").css("background-image","url(images/arrows.png)");
 		
-		$("#iphone-over").remove();
+		$("#two").addClass("hidden").removeClass("screenhot-show");
 		$("#haltestellen").css("opacity","");
 		$("#suche").css("opacity","");
 		$("#bringmehome").css("opacity","");
@@ -71,15 +93,17 @@
 	
   $("#suche").mouseover(function() {
 		$("#suche h3").css("color", "#00d9bd");
+		$("#arrows").css("z-index","-1").css("background-image","url(images/arrows-suche.png)");
 		
-		$("#iphone-center").append('<img src="images/howitworks-sucheiphone.png" id="iphone-over" style="position:absolute;width:470px;display:block;height:702px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		$("#three").removeClass("hidden").addClass("screenhot-show");
 		$("#datepicker").css("opacity","0.4");
 		$("#haltestellen").css("opacity","0.4");
 		$("#bringmehome").css("opacity","0.4");
 		$("#nextrbutton").css("opacity","0.4");
   }).mouseout(function(){
-		$("#iphone-over").remove();
+		$("#three").addClass("hidden").removeClass("screenhot-show");
 		$("#suche h3").css("color", "#000000");
+		$("#arrows").css("z-index","0").css("background-image","url(images/arrows.png)");
 		
 		$("#datepicker").css("opacity","");
 		$("#haltestellen").css("opacity","");
@@ -89,15 +113,17 @@
 	
   $("#bringmehome").mouseover(function() {
 		$("#bringmehome h3").css("color", "#00d9bd");
+		$("#arrows").css("z-index","-1").css("background-image","url(images/arrows-bringmehome.png)");
 		
-		$("#iphone-center").append('<img src="images/howitworks-bringmehomeiphone.png" id="iphone-over" style="position:absolute;width:470px;display:block;height:702px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		$("#four").removeClass("hidden").addClass("screenhot-show");
 		$("#datepicker").css("opacity","0.4");
 		$("#suche").css("opacity","0.4");
 		$("#haltestellen").css("opacity","0.4");
 		$("#nextrbutton").css("opacity","0.4");
   }).mouseout(function(){
-		$("#iphone-over").remove();
+		$("#four").addClass("hidden").removeClass("screenhot-show");
 		$("#bringmehome h3").css("color", "#000000");
+		$("#arrows").css("z-index","0").css("background-image","url(images/arrows.png)");
 		
 		$("#datepicker").css("opacity","");
 		$("#suche").css("opacity","");
@@ -107,15 +133,17 @@
 	
   $("#nextrbutton").mouseover(function() {
 		$("#nextrbutton h3").css("color", "#00d9bd");
+		$("#arrows").css("z-index","-1").css("background-image","url(images/arrows-nextr.png)");
 		
-		$("#iphone-center").append('<img src="images/howitworks-nextrbuttoniphone.png" id="iphone-over" style="position:absolute;width:470px;display:block;height:702px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		$("#five").removeClass("hidden").addClass("screenhot-show");
 		$("#datepicker").css("opacity","0.4");
 		$("#suche").css("opacity","0.4");
 		$("#bringmehome").css("opacity","0.4");
 		$("#haltestellen").css("opacity","0.4");
   }).mouseout(function(){
-		$("#iphone-over").remove();
+		$("#five").addClass("hidden").removeClass("screenhot-show");
 		$("#nextrbutton h3").css("color", "#000000");
+		$("#arrows").css("z-index","0").css("background-image","url(images/arrows.png)");
 		
 		$("#datepicker").css("opacity","");
 		$("#suche").css("opacity","");
@@ -127,7 +155,7 @@
   $("#scroll").mouseover(function() {
 		$("#scroll h3").css("color", "#00d9bd");
 		
-		$("#iphone-center2").append('<img src="images/howitworks2-coniphone-scroll.png" id="iphone-over" style="position:absolute;width:675px;display:block;height:668px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		$("#iphone-center2").append('<img src="images/howitworks2-coniphone-scroll.png" id="iphone-over" style="position:absolute;display:block;margin:0px auto;margin-top: 50px;z-index: 1;" />');
 		$("#verbindungen").css("opacity","0.4");
 		$("#detail").css("opacity","0.4");
 		$("#close").css("opacity","0.4");
@@ -144,7 +172,7 @@
   $("#detail").mouseover(function() {
 		$("#detail h3").css("color", "#00d9bd");
 		
-		$("#iphone-center2").append('<img src="images/howitworks2-coniphone-detail.png" id="iphone-over" style="position:absolute;width:675px;display:block;height:668px;margin:0px auto;margin-top: 50px;z-index: 1;" />');
+		$("#iphone-center2").append('<img src="images/howitworks2-coniphone-detail.png" id="iphone-over" style="position:absolute;display:block;margin:0px auto;margin-top: 50px;z-index: 1;" />');
 		$("#verbindungen").css("opacity","0.4");
 		$("#scroll").css("opacity","0.4");
 		$("#close").css("opacity","0.4");
@@ -157,74 +185,94 @@
 		$("#close").css("opacity","");
   });
 	
-  $("#close1").click(function()
-   {
-		 if($("#howitworks").is(":visible")){
-			 $("#howitworks").removeClass("show-on-desktop");
-			 $("#howitworks").slideUp();
-			 $("#home-close img").rotate(180);
-			 $("#home-close h1").css("color","black");
-		 }
-		 else{
-			 $("#howitworks").slideDown();
-			 $("#home-close img").rotate(0);
-			 $("#home-close h1").css("color","#a3a3a3");
-			 $("#howitworks").addClass("show-on-desktop");
-		 }
-   });
-	 
-	 
-   $("#close2").click(function()
-    {
-	 		 if($("#howitworks2").is(":visible")){
-				 $("#howitworks2").removeClass("show-on-desktop");
-	 			 $("#howitworks2").slideUp();
-				 $("#verbindungen-close img").rotate(180);
-				 $("#verbindungen-close h1").css("color","black");
-				 $("#close2").addClass("grey-grad");
-	 		 }
-	 		 else{
-	 			 $("#howitworks2").slideDown();
-				 $("#verbindungen-close img").rotate(0);
-				 $("#verbindungen-close h1").css("color","#a3a3a3");
-				 $("#close2").removeClass("grey-grad");
-				 $("#howitworks2").addClass("show-on-desktop");
-	 		 }
-    });
+ 
 	
 	
 	
-  $("#bus").mouseover(function() {
-		$("ul#bus #hover").css("display", "block");
+  $("li#bus").mouseover(function() {
+		$("li#bus .hover").css("visibility", "visible");
 	
   }).mouseout(function(){
-		$("#bus li#hover").css("display", "none");
+	$("li#bus .hover").css("visibility", "hidden");
 	 });
 	 
-   $("#sbahn").mouseover(function() {
- 		$("ul#sbahn #hover").css("display", "block");
+   $("li#sbahn").mouseover(function() {
+		$("li#sbahn .hover").css("visibility", "visible");
 	
    }).mouseout(function(){
- 		$("#sbahn li#hover").css("display", "none");
+   		$("li#sbahn .hover").css("visibility", "hidden");
  	 });
 	
-   $("#ubahn").mouseover(function() {
- 		$("ul#ubahn #hover").css("display", "block");
-	
+   $("li#ubahn").mouseover(function() {
+		$("li#ubahn .hover").css("visibility", "visible");	
    }).mouseout(function(){
- 		$("#ubahn li#hover").css("display", "none");
- 	 });
-	 
-   $("#zug").mouseover(function() {
- 		$("ul#zug #hover").css("display", "block");
-	
-   }).mouseout(function(){
- 		$("#zug li#hover").css("display", "none");
+  		$("li#ubahn .hover").css("visibility", "hidden");
  	 });
 	 
-   $("#sonstige").mouseover(function() {
- 		$("ul#sonstige #hover").css("display", "block");
-	
+   $("li#zug").mouseover(function() {
+		$("li#zug .hover").css("visibility", "visible");	
    }).mouseout(function(){
- 		$("#sonstige li#hover").css("display", "none");
+ 		$("li#zug .hover").css("visibility", "hidden");
  	 });
+	 
+   $("li#sonstige").mouseover(function() {
+		$("li#sonstige .hover").css("visibility", "visible");			
+   }).mouseout(function(){
+		$("li#sonstige .hover").css("visibility", "hidden");
+ 	});
+	
+
+	$('a[href=#features]').
+	    click(function(){
+	        var target = $('#features');
+	        if (target.length)
+	        {
+	            var top = target.offset().top;
+	            $('html,body').animate({scrollTop: top}, 1000);
+	            return false;
+	        }
+	    });
+		
+		$('a[href=#page-footer]').
+		    click(function(){
+		        var target = $('#page-footer');
+		        if (target.length)
+		        {
+		            var top = target.offset().top;
+		            $('html,body').animate({scrollTop: top}, 1000);
+		            return false;
+		        }
+		    });
+			
+			var something = document.getElementById('footer-facebook');
+
+			something.style.cursor = 'pointer';
+			something.onclick = function() {
+			    window.open("http://www.facebook.com/nextrinfo");
+			};
+			
+			var something2 = document.getElementById('footer-twitter');
+
+			something2.style.cursor = 'pointer';
+			something2.onclick = function() {
+			    window.open("http://www.twitter.com/nextrinfo");
+			};
+			
+			
+			var something = document.getElementById('ober');
+
+			something.style.cursor = 'pointer';
+			something.onclick = function() {
+			    window.open("http://www.oberhaeuser.info/");
+			};
+			
+			var something2 = document.getElementById('mwermuth');
+
+			something2.style.cursor = 'pointer';
+			something2.onclick = function() {
+			    window.open("http://www.mwermuth.com");
+			};
+			
+			
+	
+	
